1b8785a05047f4349240d24b3da94b6df9ac0f56
bejarjo <joaquin.bejar@gmail.com>
2017-11-17 12:16:21 +0100

    V0.0.3
    Git test

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ __author__ = 'Joaquin Bejar Garcia: https://www.linkedin.com/in/joaquinbejar/'
 
 import tensorflow as tf
 import keras as k
+import numpy as np
+import scipy as sp
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn
 
 
 
